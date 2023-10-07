4547920de01f063d05599c8a46675e71c14828c4
Girish-GP <113516103+Girish-GP@users.noreply.github.com>
2023-10-07 16:56:07 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs , not browser
+
+
+//code readability should be high
+console.log(3+3)
+console.log("girish")
+
+let userLoggedIn = true;
+let numberOfUsers = 30;
+let userName= "tyson";
+
+//number ==> 2 to power 53
+//null => standalone value --matlab empty
+//bigint
+//boolean => true/false
+//string => "" ''
+//symbol => unique
+//undefined => matlab abhi value assigned nahi hui hai 
+//Yeh hai hamare primitive Data Types
+
+//Object
+console.log(typeof "ggp") //string
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
