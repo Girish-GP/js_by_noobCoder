4d273441f2b22133afa1d1f92ca0aefb97d7663f
Girish-GP <113516103+Girish-GP@users.noreply.github.com>
2023-10-09 13:37:40 +0000

    Conversions

---
+++ 01_basics/03_ConversionsOperations.js
@@ -0,0 +1,32 @@
+let score = true
+
+// console.log(typeof score)
+
+let numberValue= Number(score)
+// console.log(typeof numberValue)
+// console.log(numberValue)
+
+//Conversion using Number() 
+// Innitial value , Converted value , type
+// "33" => 33 number
+// "33abc" => NaN number
+// undefined => NaN number
+// null => 0 number
+// true => 1 number
+//false=> 0 number
+
+let isLoggedIn = 1;
+let isLoggedOut = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+let booleanIsLoggedOut = Boolean(isLoggedOut);
+console.log(booleanIsLoggedIn,booleanIsLoggedOut)
+
+// 1 => true 0=> false
+// "" => false
+// "ggp" => true
+
+
+let sum = 33
+console.log(String(sum))
+console.log(typeof String(sum))
