ad2098b564d19a313957843906e4b5420dafdfa7
Girish-GP <113516103+Girish-GP@users.noreply.github.com>
2023-10-18 13:48:07 +0000

    Stack Heap memory

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,4 @@
+const name = "ggp"
+const repoCount = 50
+console.log(name + repoCount + "value")//dont use this syntax
+console.log(`${name}${repoCount}value`)//modern way using backticks ``
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -17,13 +17,13 @@ console.log(id === id2)
 
 const bigNumber = 345092201091934n
 
-console.log(typeof score)//number
-console.log(typeof scorevalue)//number
-console.log(typeof isloggedin)//boolean
-console.log(typeof outsidetemp)//object
-console.log(typeof userEmail)//undefined
-console.log(typeof id)//symbol
-console.log(typeof bigNumber)//bigint
+// console.log(typeof score)//number
+// console.log(typeof scorevalue)//number
+// console.log(typeof isloggedin)//boolean
+// console.log(typeof outsidetemp)//object
+// console.log(typeof userEmail)//undefined
+// console.log(typeof id)//symbol
+// console.log(typeof bigNumber)//bigint
 
 
 
@@ -42,5 +42,28 @@ const hello = function(){
     console.log("hello world")
 }
 
-console.log(typeof heroes, typeof obj , typeof hello)// object object function
+// console.log(typeof heroes, typeof obj , typeof hello)// object object function
 //function returns function but it is called object function
+
+
+//**************************************** *//
+
+//Stack Memory(Primitive) , Heap Memory(Non-Primitive)
+
+
+//For primitive datatypes we use stack 
+let str1 = "ggpXX"
+let str2 = str1 // here the st1 value copy is assigned to str2 so when st2 changes it does not impact str1 
+console.log(str2,str1)
+str2="XXY"
+console.log(str2,str1)
+
+
+//For non primitive datatypes we use heap
+let user1 ={
+    name:"XXX"
+}
+let user2 = user1; // here the memory reference of user1 is assigned to user2 so when user2 changes it also changes the user1
+console.log(user1, user2)
+user1.name = "TTT"
+console.log(user1, user2)
