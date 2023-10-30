3e5a41c2103db2e323f3d01b7be4d86f7f96a69e
Girish-GP <113516103+Girish-GP@users.noreply.github.com>
2023-10-30 13:52:27 +0000

    math functions

--- 01_basics/06_numbersMath.js
+++ 01_basics/06_numbersMath.js
@@ -1,25 +1,38 @@
-const score = 400 //implicit type number
-console.log(score)
+// const score = 400 //implicit type number
+// console.log(score)
 
-const balance = new Number(100)
-console.log(balance,typeof balance) //[Number: 100] object
+// const balance = new Number(100)
+// console.log(balance,typeof balance) //[Number: 100] object
 
-console.log(balance.toString())
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))//precision value 100.00
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))//precision value 100.00
 
-const otherNumber = 23.8978
-console.log(otherNumber.toPrecision(2)) //will convert 23.8978 to a precised 2 digit only with .8 rounding off to 24
-console.log(otherNumber.toPrecision(3))//23.9
+// const otherNumber = 23.8978
+// console.log(otherNumber.toPrecision(2)) //will convert 23.8978 to a precised 2 digit only with .8 rounding off to 24
+// console.log(otherNumber.toPrecision(3))//23.9
 
-const num1 = 20;
-let num2 = num1;
-console.log(num1,num2)
-num2= 45;
-console.log(num1,num2)
+// const num1 = 20;
+// let num2 = num1;
+// console.log(num1,num2)
+// num2= 45;
+// console.log(num1,num2)
 
-const num3 = new Number(30)
-let num4 = num3
-console.log(num3,num4)
-num4 = new Number(67)
-console.log(num3,num4)
+// const num3 = new Number(30)
+// let num4 = num3
+// console.log(num3,num4)
+// num4 = new Number(67)
+// console.log(num3,num4)
+
+
+///Maths
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(3.7))
+// console.log(Math.ceil(4.3)) // round to highest value
+// console.log(Math.floor(4.9)) // roundoff to lowest value
+// console.log(Math.min(4,33,42,45,2))
+// console.log(Math.max(4,33,42,45,2))
+
+console.log(Math.random())// 0 - 1
+console.log(Math.floor((Math.random()*10)) + 1) // 1 - 10
