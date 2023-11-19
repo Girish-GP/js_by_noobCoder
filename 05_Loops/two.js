b849c4678ca453eb79f1d8380ba79b6f2495f77f
Girish <girishparulekar19@gmail.com>
2023-11-19 21:03:20 +0530

    while , do while loops

---
+++ 05_Loops/two.js
@@ -0,0 +1,18 @@
+//While loop , Do while loop
+let index = 10
+// while(index > 0){
+//     console.log(`Value: ${index}`)
+//     index--;
+// }
+
+const arr = ['flash','batman','superman']
+let count = 0
+while(count<arr.length){
+    console.log(`Hero ${count+1} is ${arr[count]}`)
+    count++
+}
+
+
+// do{
+//     console.log(`Value: ${index}`)
+// }while(index>10)
