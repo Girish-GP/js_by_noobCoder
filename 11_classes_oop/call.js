480f2709c65fd52f2d6de9ca6eed6fda02a5f73c
Girish-GP <113516103+Girish-GP@users.noreply.github.com>
2024-02-04 12:01:47 +0000

    call in js

---
+++ 11_classes_oop/call.js
@@ -0,0 +1,23 @@
+//In JavaScript, the call() method is a function method that allows you to call a function with a specified this value and arguments provided individually. It is used to invoke a function and explicitly set the context (the value of this) for that function.
+
+//idhar setIuserName ka joh bhi context hai usse ham ek explicit this object se attach karenge
+
+//The first argument to call is the context object (this value), and subsequent arguments are passed as individual arguments to the function.
+
+function setIUserName(userName){
+  //Complex DB call
+  this.userName = userName
+}
+
+function createUser(userName,password,age){
+    // setIUserName(userName) // scenario 1
+    setIUserName.call(this,userName) // scenario 2
+    //Idhar hamne call use karke setIusername ko createUser ka this refer karne ko bola because of the userName which is set in setIUserName function will be attach to this of createUser function
+    this.password = password
+    this.age = age
+}
+
+const user = new createUser('GGP','2233',24)
+
+console.log(user) //createUser { password: '2233', age: 24 } scenario 1
+//createUser { userName: 'GGP', password: '2233', age: 24 } scenario 2
--- 11_classes_oop/object.js
+++ 11_classes_oop/object.js
@@ -1,29 +1,48 @@
-function multiply(num){
-    return num*3
-}
+// function multiply(num){
+//     return num*3
+// }
 
-multiply.power = 2;
+// multiply.power = 2;
 
-console.log(multiply(5))
-console.log(multiply.power)
-console.log(multiply.prototype)
+// console.log(multiply(5))
+// console.log(multiply.power)
+// console.log(multiply.prototype)
 
-function createUser(username,score){
-    this.username  = username;
-    this.score = score;
-}
+// function createUser(username,score){
+//     this.username  = username;
+//     this.score = score;
+// }
 
-createUser.prototype.increment = function(){
-    this.score++;
-    return this.score
-}
+// createUser.prototype.increment = function(){
+//     this.score++;
+//     return this.score
+// }
 
-createUser.prototype.printMe = function(){
-    console.log(`Score is ${this.score}`)
-}
+// createUser.prototype.printMe = function(){
+//     console.log(`Score is ${this.score}`)
+// }
 
-const user1 = new createUser('xyz',23)
-const user2 = new createUser('ggp',12)
+// const user1 = new createUser('xyz',23)
+// const user2 = new createUser('ggp',12)
 
-console.log(user1.increment())
-console.log(user2.increment())
+// console.log(user1.increment())
+// console.log(user2.increment())
+
+const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,101,102,103,104,105,106,107,108,109,110]
+const divisibleBy = [2,3,4,5,6,7,8,9]
+const temp =arr.reduce((accumulator,currentValue)=>{
+    let isPrime = true;
+  divisibleBy.forEach((item)=>{
+     if(currentValue > 1 && currentValue !== item && currentValue % item === 0){
+        isPrime = false
+     } 
+
+  })
+  if(isPrime){
+    accumulator.push(currentValue)
+  } 
+
+  return accumulator;
+},[])
+
+console.log(temp)
